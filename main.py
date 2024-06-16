5b4fef6577635ec9fe14f4702cd8ad040972babc
anushap6571 <146144960+anushap6571@users.noreply.github.com>
2024-06-16 15:07:59 -0500

    better math and function use

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ hour = t.tm_hour%12
 minute = t.tm_min
 
 # calculate angle of both hour hand and minute hand
-hour_angle = ((hour * 30) + (minute * 0.5)) % 360
-min_angle = minute * 6
+hour_angle = 90 - ((hour * 30) + (minute * 0.5)) % 360
+min_angle = 90 - ((minute * 6) %360)
 print(hour_angle)
 print(min_angle)
+
 # draw circle for clock
 theta = np.linspace( 0 , 2 * np.pi , 150 )
 radius = 0.4
@@ -22,30 +23,40 @@ figure, axes = plt.subplots( 1 )
 axes.plot( a, b )
 axes.set_aspect( 1 )
 
-# set an hour hand length
+
+
+def drawLine(len, angle, x1, y1):
+    # calculate x and y coordinates
+    x2 = len * math.cos((angle) * math.pi/180) 
+    y2 = len * math.sin((angle) * math.pi/180)
+
+    # plot the hour hand line
+    plt.plot([x1,x2], [y1, y2])
+
+
+# set an hour and minute hand length
 hour_len = 0.15
-
-# calculate x and y coordinates 
-hour_x = hour_len * math.cos((hour_angle%90) * math.pi/180) 
-hour_x = hour_x * -1 if (hour_angle/90) > 1 else hour_x
-hour_y = hour_len * math.sin((hour_angle%90) * math.pi/180)
-hour_y = hour_y * -1 if (hour_angle/90) == 1 or (hour_angle/90) == 2 else hour_y
-
-print(hour_x)
-print(hour_y)
-
-# plot the hour hand line
-plt.plot([0,hour_x], [0, hour_y])
-
 min_len = hour_len*2
 
-min_x = min_len * math.cos((min_angle%90) * math.pi/180) 
-min_x = min_x * -1 if (minute%15) > 1 else min_x
-min_y = min_len * math.sin((min_angle%90) * math.pi/180)
-min_y = min_y * -1 if (minute%15) == 1 or (minute%15) == 2 else min_y
+drawLine(hour_len, hour_angle, 0, 0)
+drawLine(min_len, min_angle, 0, 0)
 
-# plot the minute hand line
-plt.plot([0,min_x], [0, min_y])
+
+for i in range(0, 59):
+    if(i%5 == 0):
+        len = .01
+    else:
+        len = 0.005
+    angle = i * 6
+    x1 = 0.001*math.cos(angle * math.pi/180)
+    y1 = 0.001*math.sin(angle * math.pi/180)
+    drawLine(len, angle, x1, y1)
+
+
+
+
+
+
 
 
 
